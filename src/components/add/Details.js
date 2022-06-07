4b494e00744aa05eb2e697f8bcf14882b0dead7c
pedrompena <pedrompena07@gmail.com>
2022-06-07 16:39:46 +0200

    Details Styles

--- src/components/add/Details.js
+++ src/components/add/Details.js
@@ -1,46 +1,48 @@
+import styles from './Details.module.css'
+
 const Details = () => {
 
     const frequencyOptions = ["day", "week", "month", "year"];
     const iconOptions = ["💻", "🏃", "📚", "✈️", "💵"];
 
     return (
-        <div>
-            <form>
-                <label>
+        <div className='card'>
+            <form className='p-4'>
+                <label className='label'>
                     Describe your goal.
-                    <input placeholder="ex. 52 walks"/>
+                    <input placeholder="ex. 52 walks" className='input' />
                 </label>
-                <label>
+                <label className='label'>
                     How often do you want to meet your goal?<span>(ex. 1 time per week)</span>
-                    <div>
-                        <input type="number" />
-                        <select>
+                    <div className='flex mb-6'>
+                        <input type="number" className='input mr-6' />
+                        <select className='input'>
                             {frequencyOptions.map(option => <option value={option}>{option}</option>)}
                         </select>
                     </div>
                 </label>
-                <label>
+                <label className='label'>
                     How many times do you want to complete this goal?
-                    <input type="number" />
+                    <input type="number" className='input' />
                 </label>
-                <label>
+                <label className='label'>
                     Do you have a deadline to meet your goal?
-                    <input type="date" />
+                    <input type="date" className='input' />
                 </label>
-                <label>
+                <label className='label'>
                     How many times have you completed this goal?
-                    <input type="number" />
+                    <input type="number" className='input' />
                 </label>
-                <label>
+                <label className='label'>
                     Choose an icon for your goal
-                    <select>
-                    {iconOptions.map(option => <option value={option}>{option}</option>)}
+                    <select className='input'>
+                        {iconOptions.map(option => <option value={option}>{option}</option>)}
                     </select>
                 </label>
             </form>
-            <div>
-                <button>Create</button>
-                <button>Cancel</button>
+            <div className='buttons'>
+                <button className='button button--black'>Create</button>
+                <button className='button button--gray'>Cancel</button>
             </div>
         </div>
     );
